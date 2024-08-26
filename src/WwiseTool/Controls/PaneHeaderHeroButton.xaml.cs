a325154cf62faa5748b1851eb6dd1aba9475d205
xezrunner <8061077+xezrunner@users.noreply.github.com>
2024-08-27 01:33:46 +0200

    ui: ProjectPage tweaks

--- src/WwiseTool/Controls/PaneHeaderHeroButton.xaml.cs
+++ src/WwiseTool/Controls/PaneHeaderHeroButton.xaml.cs
@@ -1,8 +1,6 @@
 using System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace WwiseTool.Controls {
     public sealed partial class PaneHeaderHeroButton : UserControl {
